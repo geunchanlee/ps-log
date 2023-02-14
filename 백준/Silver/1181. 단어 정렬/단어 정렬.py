61c92158a27128582e951618c7523f65411d6181
근찬 <rmscks97@inu.ac.kr>
2023-02-14 18:56:03 +0900

    [Silver V] Title: 단어 정렬, Time: 828 ms, Memory: 35868 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,11 @@
+N = int(input())
+word = []
+for i in range(N):
+    word.append(input())
+    
+word = list(set(word))
+word.sort()
+word.sort(key=lambda x: len(x))
+
+for i in word:
+    print(i)
