dc4b1a00874e2910a0b10efc2975efd7ba802cfd
근찬 <rmscks97@inu.ac.kr>
2023-07-27 15:38:33 +0900

    [Bronze II] Title: 소수, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,15 +1,16 @@
-N = int(input())
-M = int(input())
+N, M = int(input()), int(input())
+# 에라토스테네스의 체 사용
+P = [True] * (M+1)
+P[1] = False
+for i in range(2, int(M**0.5) + 1):
+    if P[i] == True:
+        for j in range(i + i, M + 1, i):
+            P[j] = False
 
-p = []
-for i in range(N, M + 1):
-    for j in range(2, i + 1):
-        if j == i:
-            p.append(i)
-        elif i % j == 0:
-            break
-if len(p) == 0:
+ans = [i for i in range(N, M + 1) if P[i] == True]
+
+if len(ans) == 0:
     print(-1)
 else:
-    print(sum(p))
-    print(p[0])
+    print(sum(ans))
+    print(ans[0])
