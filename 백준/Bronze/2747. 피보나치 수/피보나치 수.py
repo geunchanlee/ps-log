33580b81f52ca402617553c3b4cbf5b98a97e0aa
근찬 <rmscks97@inu.ac.kr>
2024-08-05 16:28:15 +0900

    [Bronze II] Title: 피보나치 수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,12 @@
+n = int(input())
+x, y = 0, 1
+if n < 2:
+    print(n)
+else:
+    for i in range(n-1):
+        z = x + y
+        if i % 2 == 0:
+            x = z
+        else:
+            y = z
+    print(z)
