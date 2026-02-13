5fdf7721ca3cbd70a243e21e079fe45fac8c7d9d
근찬 <geunchanlee.dev@gmail.com>
2026-02-13 16:17:14 +0900

    [D1] Title: 최대수 구하기, Time: 89 ms, Memory: 26,880 KB -BaekjoonHub

---
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int max = 0;
+            for (int i=0; i<10; i++){
+                int n = sc.nextInt();
+                if (n > max) {
+                    max = n;
+                }
+            }
+            System.out.print("#"+test_case+" ");
+            System.out.println(max);
+        }
+    }
+}
